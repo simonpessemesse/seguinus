c6a68e928ebf8c24d2d9dcdf1f315f51507306da
unsigned <unsigned@wouhou>
2015-12-25 21:03:12 +0100

    settings

--- seguinus/settings.py
+++ seguinus/settings.py
@@ -68,7 +68,8 @@ WSGI_APPLICATION = 'seguinus.wsgi.application'
 DATABASES = {
     'default': {
 		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
+		'NAME': os.path.join(BASE_DIR,'db.sqlite3'),                      # Or
+		path to database file if using sqlite3.
 		'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
