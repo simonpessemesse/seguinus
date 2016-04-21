64229ab145379a2174ee481feaf7f5d9f9a4bc9f
simon pessemesse <pessemesse@free.fr>
2016-04-21 12:30:43 +0200

    init changed with user/password set as test/test

--- init.bash
+++ init.bash
@@ -1,6 +1,12 @@
 cd seguinus
-cp -nv settings.py.example settings.py
+if [ ! -f settings.py ]; then
+    echo "copy settings"
+    cp -nv settings.py.example settings.py
+fi
 cd ..
-cp -nv preferences.py.example preferences.py 
+if [ ! -f preferences.py ]; then
+    echo "copy preferences"
+    cp -nv preferences.py.example preferences.py 
+fi
 python manage.py migrate
-python manage.py loaddata initData.json # TODO only execute this if there was no previous settings.py
+python manage.py loaddata initData.json 
